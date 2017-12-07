94a89bc9c8c9ea453614478828be9d26aebddc50
C9-LinkRs <linkrsfan@gmail.com>
2017-12-07 18:43:51 -0500

    Codeforces 596B added

--- Codeforces 596B.cpp
+++ Codeforces 596B.cpp
@@ -1,18 +1,16 @@
-#include <iostream>
+#include <stdio.h>
 #include <cmath>
 
 using namespace std;
 
 int main(){
-    int n, sum, delta;
-    cin >> n;
+    int n;
+    long long sum;
+    scanf("%d",&n);
     int b[n];
-    for(int i = 0; i < n; i++) cin >> b[i];
-    sum = b[0];
-    for(int i = 1; i < n; i++){
-        delta=b[i]-b[i-1];
-        sum+=abs(delta);
-    }
-    printf("%d\n", sum);
+    for(int i = 0; i < n; i++) scanf("%d", &b[i]);
+    sum = abs(b[0]);
+    for(int i = 1; i < n; i++) sum+=abs(b[i]-b[i-1]);
+    printf("%lld\n", sum);
     return 0;
 }
