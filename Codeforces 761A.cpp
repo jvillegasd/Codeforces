eac9a6a0a9450ae44fc082cd8c173b10361f59a4
C9-LinkRs <linkrsfan@gmail.com>
2017-12-20 00:11:02 -0500

    Codeforces 761A added

---
+++ Codeforces 761A.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if(a == 0 && b == 0) printf("NO\n");
+    else if(abs(a - b) <= 1) printf("YES\n");
+    else printf("NO\n");
+    return 0;
+}
