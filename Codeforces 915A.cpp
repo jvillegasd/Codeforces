c8134e6a3a737204bee996089139db2913275170
C9-LinkRs <linkrsfan@gmail.com>
2018-01-17 18:39:42 -0500

    Codeforces 915A added

--- Codeforces 915A.cpp
+++ Codeforces 915A.cpp
@@ -7,7 +7,7 @@ int main(){
     scanf("%d %d", &n, &k);
     for(int i = 0; i < n; i++){
         scanf("%d", &input);
-        if(input <= k && k % input == 0 && input > max && k >= input) max = input;
+        if(input <= k && k % input == 0 && input > max) max = input;
     }
     if(k != 1) printf("%d\n", k/max);
     else printf("1\n");
