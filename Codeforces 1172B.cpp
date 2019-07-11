864636ce1656c143d28195c67ad7c8f2d9370191
Johnny Villegas <linkrsfan@gmail.com>
2019-07-11 00:13:38 -0500

    Add files via upload

---
+++ Codeforces 1172B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define fp first
+#define sp second
+#define mp make_pair
+#define ins insert
+#define uEdge(u, v) g[u].pb(v), g[v].pb(u)
+#define uwEdge(u, v, w) g[u].pb({v, w}), g[v].pb({u, w})
+#define dEdge(u, v) g[u].pb(v)
+#define dwEdge(u, v, w) g[u].pb({v, w})
+#define BOOST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define minHeap int, vector<int>, greater<int>
+#define MOD 998244353
+
+using namespace std;
+typedef long long int lli;
+typedef pair<int, int> pii;
+lli nl, arr[0];
+vector<int> g[0];
+
+lli gcd(lli a, lli b){if(b == 0) return a;a%=b;return gcd(b, a);}
+lli lcm(lli a, lli b){return (a*b)/gcd(a, b);}
+int bs(lli sum){int l=0,r=nl-1,mid;while(l<=r){mid=(l+r)/2;if(arr[mid]==sum)return mid;if(arr[mid]<sum)l=mid+1;else r=mid-1;}return -1;}
+lli modPow(lli a, lli b){a%=MOD;lli ans=1;while(b > 0){if(b & 1) ans=ans*a%MOD;a=a*a%MOD;b>>=1;}return ans;}
+
+int main(){
+    int n, u, v, deg[200010];
+    lli ans=1, fac[200010];
+    fac[1]=1;
+    scanf("%d", &n);
+    for(int i = 1; i < n; i++) scanf("%d %d", &u, &v), deg[u]++, deg[v]++, fac[i+1]=fac[i]*(i+1)%MOD;
+    for(u = 1; u <= n; u++) ans=ans*fac[deg[u]]%MOD;
+    printf("%lld", ans*n%MOD);
+    return 0;
+}
